133485dc346dfa06ca14f64cdca2402c9053907e
JakubKomorowski <jakub.komorowski01@gmail.com>
2021-06-07 02:34:25 +0200

    initial commit with working formatted data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+}));
+
+const App = () => {
+  const [usersData, setUsersData] = useState([]);
+  const [formattedData, setFormattedData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getApi = () => {
+    axios
+      .get(
+        `https://xtramile.azure-api.net/stats/lukaszcoding?apiSecret=${process.env.REACT_APP_API_SECRET}`
+      )
+      .then(({ data }) => {
+        setUsersData(data);
+        setLoading(false);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  const formatData = () => {
+    //creating not repeating emails + anonymous user array
+    const usersEmailsArray = [
+      ...new Set(
+        usersData.map(({ email, person }) =>
+          email.length !== 0 ? email : person
+        )
+      ),
+    ];
+
+    const newData = usersEmailsArray.map((email) => {
+      let userProjects = [];
+
+      // creating not repeating projects array
+      usersData.forEach((item) => {
+        if (item.email === email || email === "Anonymous user") {
+          userProjects = [...new Set([...userProjects, item.project])];
+        }
+      });
+
+      // creating project and course arrays
+      userProjects = userProjects.map((project) => {
+        let projectObj = {};
+        let tempCourses = [];
+        let tempLessonsOpen = [];
+        let tempLessonsCompleted = [];
+
+        //when email and project exist in the object we find and add courses with lessons opened and completed
+        usersData.forEach((item) => {
+          if (
+            (item.project === project && item.email === email) ||
+            (item.project === project &&
+              email === "Anonymous user" &&
+              item.person === "Anonymous user")
+          ) {
+            tempCourses = [...tempCourses, item.course];
+            tempLessonsOpen = [...tempLessonsOpen, item.openedLessonsCount];
+            tempLessonsCompleted = [
+              ...tempLessonsCompleted,
+              item.completedLessonsCount,
+            ];
+            // creating a course object
+            const newTempCourses = tempCourses.map((el, i) => {
+              const courseObj = {
+                courseName: el,
+                lessonsOpen: parseInt(tempLessonsOpen[i]),
+                lessonsCompleted: tempLessonsCompleted[i],
+              };
+              return courseObj;
+            });
+            //creating project object
+            projectObj = {
+              projectName: project,
+              courses: newTempCourses,
+            };
+          }
+        });
+
+        return projectObj;
+      });
+
+      //removing empty objects from projects array
+      const newUsersProjects = userProjects.filter(
+        (value) => Object.keys(value).length !== 0
+      );
+
+      const dataItem = {
+        email,
+        newUsersProjects,
+      };
+      return dataItem;
+    });
+
+    console.log("TUTAJ", newData);
+    setFormattedData(newData);
+  };
+
+  //formatData triggers when usersData exists
+  useEffect(() => {
+    formatData();
+  }, [usersData]);
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading ? (
+        <Backdrop className={classes.backdrop} open>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      ) : (
+        <ul>
+          {formattedData.map((user, index) => {
+            return (
+              <li key={index}>
+                <p>{user.email}</p>
+                --------------------
+                {user.newUsersProjects.map((project) => {
+                  return (
+                    <li>
+                      <p>{project.projectName}</p>
+                    </li>
+                  );
+                })}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
   );
-}
+};
 
 export default App;
